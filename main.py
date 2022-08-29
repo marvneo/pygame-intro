96e0ee357cfd6ac62dd0fa7c419440f52bfabcaa
marvneo <60399677+marvneo@users.noreply.github.com>
2022-08-29 13:53:07 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1920,1080))
+
+pygame.display.set_caption('Python Game')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
