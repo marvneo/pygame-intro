2dbf7c9fdb72498319eb0f0611a78328cfe29ea0
marvneo <60399677+marvneo@users.noreply.github.com>
2022-08-30 10:21:33 -0700

    adeed player

--- main.py
+++ main.py
@@ -16,9 +16,17 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 # creates surface
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('Hello world', False, 'yellow')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('Hello world', False, 'yellow').convert()
+
+# snail
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_pos = 850
+snail_accel = 1
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(topleft = (80,200))
 
 while True:
     for event in pygame.event.get():
@@ -29,6 +37,16 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface, (300,350))
+    screen.blit(snail_surface, (snail_pos,260))
+    snail_pos -= snail_accel
+
+    # loops snail when it leaves the screen
+    if -150 > snail_pos:
+        snail_pos = 850
+
+    # player
+    screen.blit(player_surface, player_rect)
+
 
     pygame.display.update()
     clock.tick(60)
