b17f63e3ffab42e46d33f2e1f70098a5b2bb7999
marvneo <60399677+marvneo@users.noreply.github.com>
2022-08-30 09:11:08 -0700

    commit efore adding graphics

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import pygame
 from sys import exit
 
 pygame.init()
+
+# creates display surface
 screen = pygame.display.set_mode((1920,1080))
 
 pygame.display.set_caption('Python Game')
 
 clock = pygame.time.Clock()
 
+# creates surface
+test_surface = pygame.Surface((300,500))
+test_surface.fill('white')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface,(200,100))
 
     pygame.display.update()
     clock.tick(60)
