d481dfa770adc12c179c259893b00e48e939faaf
marvneo <60399677+marvneo@users.noreply.github.com>
2022-08-30 09:37:28 -0700

    add title text

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from sys import exit
 pygame.init()
 
 # creates display surface
-screen_width, screen_length = 800,450
+screen_width, screen_length = 800,400
 screen = pygame.display.set_mode((screen_width,screen_length))
 
+# adds title
 pygame.display.set_caption('Python Game')
 
+# necessary for frame rate control
 clock = pygame.time.Clock()
+
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 # creates surface
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('Hello world', False, 'yellow')
 
 while True:
     for event in pygame.event.get():
@@ -22,7 +27,8 @@ while True:
             exit()
 
     screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0,screen_length-168))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface, (300,350))
 
     pygame.display.update()
     clock.tick(60)
