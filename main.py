df370a2bc764794962f62563ebf7c19232beb22c
marvneo <60399677+marvneo@users.noreply.github.com>
2022-08-31 08:38:59 -0700

    add intro screen and functional score tracking

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pygame
 from sys import exit
 
 def display_score():
-    curr_time = pygame.time.get_ticks()
-    score_surface = test_font.render('Score:'+ f'{curr_time}', False, (64,64,64)).convert()
+    curr_time = pygame.time.get_ticks() - start_time
+    score = curr_time // 100
+    score_surface = test_font.render('Score: '+ f'{score}', False, (64,64,64)).convert()
     score_rect = score_surface.get_rect(topleft = (30,30))
     screen.blit(score_surface, score_rect)
 
@@ -14,34 +15,35 @@ pygame.init()
 # creates display surface
 screen_width, screen_length = 800,400
 screen = pygame.display.set_mode((screen_width,screen_length))
-
 # adds title
 pygame.display.set_caption('Python Game')
-
 # necessary for frame rate control
 clock = pygame.time.Clock()
-
-game_active = True
-
+game_active = False
+start_time = 0
 # font type
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+# intro screen
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand_rect = player_stand.get_rect(center = (400,200))
 
 # creates surface
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-
 # snail
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_pos = 800
 snail_rect = snail_surface.get_rect(midbottom = (snail_pos,300))
-snail_accel = 3
+snail_accel = 4
 # player
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80,300))
 player_gravity = 0
+
 # game over
 game_over_text = test_font.render('GAME OVER', False, 'red').convert()
 game_over_rect = game_over_text.get_rect(center =(400,200))
+game_over = False
 
 # runs game
 while True:
@@ -62,14 +64,14 @@ while True:
         if not game_active:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
+                game_over = False
+                start_time = pygame.time.get_ticks()
                 snail_rect.left = 900
 
     if game_active:
         # renders animation
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface,(0,300))
-        #pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        #pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
         display_score()
         # snail movement
         snail_rect.left -= snail_accel
@@ -88,14 +90,20 @@ while True:
         # handles collisions
         if snail_rect.colliderect(player_rect):
             game_active = False
+            game_over = True
+
 
     else:
-        screen.blit(game_over_text, game_over_rect)
+        if game_over:
+            screen.blit(game_over_text, game_over_rect)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+        else:
+            screen.fill((94, 129, 162))
+            screen.blit(player_stand, player_stand_rect)
 
 
 
